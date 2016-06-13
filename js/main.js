7a7bfe202e51be140df5de4de411b0078981dd64
TanyuS <tanyusokolova@gmail.com>
2016-06-13 09:59:08 +0500

    Content block
    datepicker timer, mark up, styles

--- js/main.js
+++ js/main.js
@@ -2,5 +2,6 @@
 (function($){
     $(window).load(function(){
         $(".sidebar__menu").mCustomScrollbar({scrollButtons:{enable:true}});
+   		$(":date").dateinput({format: "dd.mm.yyyy"});
     });
 })(jQuery);
